135b37061308b753fbb7afc8a84483b2e94d0e7c
Jared Sprague <jared@caramelcode.com>
2018-09-09 23:14:41 -0400

    playing around with tiled and phaser3

---
+++ draft/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background" tilewidth="32" tileheight="32" tilecount="7200" columns="100">
+ <image source="livingRoom.jpg" width="3201" height="2318"/>
+</tileset>
