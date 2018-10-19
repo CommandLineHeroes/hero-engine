b902cff00d82e31743ca68cdaaf3a356cca74bbc
Michael Clayton <mclayton@redhat.com>
2018-10-19 14:03:00 -0400

    loosen line length linting & fix main_text lint

--- examples/reference_room/main_text/main_text.js
+++ examples/reference_room/main_text/main_text.js
@@ -1,26 +1,28 @@
 const config = {
-    type  : Phaser.AUTO,
-    width : 1200,
+    type: Phaser.AUTO,
+    width: 1200,
     height: 929,
-    scene : {
+    scene: {
         preload: preload,
-        create : create,
-    },
+        create: create
+    }
 };
 
-var game = new Phaser.Game(config);
-var text = null;
+new Phaser.Game(config);
 let map = null;
 var graphics;
 
 function preload() {
-    //TODO: Add Dialog Manager init here
+    // TODO: Add Dialog Manager init here
     // load the tileset image which is just a single tile for the background room
     this.load.image('room1_img', '../../assets/images/room1.png');
     this.load.image('window', '../../assets/images/window.png');
 
     // load the tilemap exported from Tiled
-    this.load.tilemapTiledJSON('map', '../../assets/tilemaps/roomwithobject.json');
+    this.load.tilemapTiledJSON(
+        'map',
+        '../../assets/tilemaps/roomwithobject.json'
+    );
 }
 
 function create() {
@@ -53,10 +55,10 @@ function checkObjectSelection(pointer) {
         if (object.hasOwnProperty('polyline')) {
             // each point on the polyline is relative to it's parent object's position,
             // so need to map them to world values
-            let points = object.polyline.map((value) => {
+            let points = object.polyline.map(value => {
                 return {
                     x: value.x + object.x,
-                    y: value.y + object.y,
+                    y: value.y + object.y
                 };
             });
             let polygon = new Phaser.Geom.Polygon(points);
@@ -65,30 +67,48 @@ function checkObjectSelection(pointer) {
                 selectedObject = object;
                 break;
             }
-        }
-        else {
+        } else {
             // not a polygon, so can just check against x,y,width,height
-            if (x >= object.x && x <= object.x + object.width
-                && y >= object.y - object.height && y <= object.y) {
+            if (
+                x >= object.x &&
+                x <= object.x + object.width &&
+                y >= object.y - object.height &&
+                y <= object.y
+            ) {
                 selectedObject = object;
                 break;
             }
         }
     }
     if (selectedObject) {
-        //TODO: Dialog creation should happen here per requirements. We should also destroy or hide any other dialog currently displayed
-        //// WARNING: Hardcoding a minimal textbox to display.
+        // TODO: Dialog creation should happen here per requirements. We should also destroy or hide any other dialog currently displayed
+        // // WARNING: Hardcoding a minimal textbox to display.
         graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
         let topPadding = 50;
         let leftPadding = 40;
-        dialogBox = new Phaser.Geom.Rectangle(config.width-(config.width*11/12), config.height-(config.height*5/16), config.width*5/6, config.width*1/6);
+        var dialogBox = new Phaser.Geom.Rectangle(
+            config.width - (config.width * 11) / 12,
+            config.height - (config.height * 5) / 16,
+            (config.width * 5) / 6,
+            (config.width * 1) / 6
+        );
         graphics.fillRectShape(dialogBox);
         var style = {
-          font: "24px Arial",
-          fill: "#ff0044",
-          align: "center",
-          "backgroundColor" : "#ffffff"};
-        var text = this.add.text(dialogBox.x+leftPadding, dialogBox.y+topPadding, 'selected object description: ' + selectedObject.properties['description'], style);
-        console.log('selected object description:' + selectedObject.properties['description']);
+            font: '24px Arial',
+            fill: '#ff0044',
+            align: 'center',
+            backgroundColor: '#ffffff'
+        };
+        this.add.text(
+            dialogBox.x + leftPadding,
+            dialogBox.y + topPadding,
+            'selected object description: ' +
+                selectedObject.properties['description'],
+            style
+        );
+        console.log(
+            'selected object description:' +
+                selectedObject.properties['description']
+        );
     }
 }
