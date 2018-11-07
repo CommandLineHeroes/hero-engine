6dcf31cc9f4e64ab4f635fca1eab5db62df02102
Michael Clayton <mclayton@redhat.com>
2018-11-07 13:34:57 -0500

    on examples, run prettier but not eslint

--- examples/reference_room/activate_object/activate_object.js
+++ examples/reference_room/activate_object/activate_object.js
@@ -13,41 +13,41 @@ let map = null;
 
 function preload() {
     // load the tileset image which is just a single tile for the background room
-    this.load.image('room1_img', '../../assets/images/room1.png');
-    this.load.image('window', '../../assets/images/window.png');
-    this.load.image('light_switch', '../../assets/images/button.png');
-    this.load.image('light_off', '../../assets/images/light_off.png');
-    this.load.image('light_on', '../../assets/images/light_on.png');
+    this.load.image("room1_img", "../../assets/images/room1.png");
+    this.load.image("window", "../../assets/images/window.png");
+    this.load.image("light_switch", "../../assets/images/button.png");
+    this.load.image("light_off", "../../assets/images/light_off.png");
+    this.load.image("light_on", "../../assets/images/light_on.png");
 
     // load the tilemap exported from Tiled
     this.load.tilemapTiledJSON(
-        'map',
-        '../../assets/tilemaps/roomwithobject.json'
+        "map",
+        "../../assets/tilemaps/roomwithobject.json"
     );
 }
 
 function create() {
     // parse the tilemap
-    map = this.make.tilemap({ key: 'map' });
+    map = this.make.tilemap({ key: "map" });
 
     // add the tileset image to the map
-    let tiles = map.addTilesetImage('room1', 'room1_img');
-    map.addTilesetImage('window', 'window');
-    map.addTilesetImage('light_switch', 'light_switch');
+    let tiles = map.addTilesetImage("room1", "room1_img");
+    map.addTilesetImage("window", "window");
+    map.addTilesetImage("light_switch", "light_switch");
 
     // add the room_map layer to the scene
-    map.createStaticLayer('room_map', tiles, 0, 0);
+    map.createStaticLayer("room_map", tiles, 0, 0);
     // create sprites for the objects layer
-    map.createFromObjects('objects', 'window', { key: 'window' });
-    map.createFromObjects('objects', 'light_switch', { key: 'light_switch' });
+    map.createFromObjects("objects", "window", { key: "window" });
+    map.createFromObjects("objects", "light_switch", { key: "light_switch" });
 
-    map.createFromObjects('objects', 'ceiling_light', { key: 'light_off' });
+    map.createFromObjects("objects", "ceiling_light", { key: "light_off" });
 
-    this.input.on('pointerup', checkObjectSelection, this);
+    this.input.on("pointerup", checkObjectSelection, this);
 }
 
 function checkObjectSelection(pointer) {
-    let objectLayer = map.getObjectLayer('objects');
+    let objectLayer = map.getObjectLayer("objects");
     if (!objectLayer) return;
 
     // ensure we take the camera position into account
@@ -57,7 +57,7 @@ function checkObjectSelection(pointer) {
     let selectedObject = null;
     for (let object of objectLayer.objects) {
         // check if object is a polygon
-        if (object.hasOwnProperty('polyline')) {
+        if (object.hasOwnProperty("polyline")) {
             // each point on the polyline is relative to it's parent object's position,
             // so need to map them to world values
             let points = object.polyline.map(value => {
@@ -86,24 +86,24 @@ function checkObjectSelection(pointer) {
         }
     }
     if (selectedObject) {
-        if (selectedObject.properties['description']) {
+        if (selectedObject.properties["description"]) {
             console.log(
-                'selected object description:' +
-                    selectedObject.properties['description']
+                "selected object description:" +
+                    selectedObject.properties["description"]
             );
-        } else if (selectedObject.properties['onUse']) {
+        } else if (selectedObject.properties["onUse"]) {
             objectTrigger(selectedObject, this);
         }
     }
 }
 
 function objectTrigger(object, scene) {
-    if (object.properties['onUse'] === 'toggle ceiling_light') {
-        console.log('toggling ceiling light');
-        let light = scene.children.list.find(o => o.name === 'ceiling_light');
+    if (object.properties["onUse"] === "toggle ceiling_light") {
+        console.log("toggling ceiling light");
+        let light = scene.children.list.find(o => o.name === "ceiling_light");
         if (light) {
             let textureKey =
-                light.texture.key === 'light_off' ? 'light_on' : 'light_off';
+                light.texture.key === "light_off" ? "light_on" : "light_off";
             light.setTexture(textureKey);
         }
     }
--- examples/reference_room/main_text/main_text.js
+++ examples/reference_room/main_text/main_text.js
@@ -1,11 +1,11 @@
 const config = {
-    type  : Phaser.AUTO,
-    width : 1200,
+    type: Phaser.AUTO,
+    width: 1200,
     height: 929,
-    scene : {
+    scene: {
         preload: preload,
-        create : create,
-    },
+        create: create
+    }
 };
 
 var game = new Phaser.Game(config);
@@ -16,31 +16,34 @@ var graphics;
 function preload() {
     //TODO: Add Dialog Manager init here
     // load the tileset image which is just a single tile for the background room
-    this.load.image('room1_img', '../../assets/images/room1.png');
-    this.load.image('window', '../../assets/images/window.png');
+    this.load.image("room1_img", "../../assets/images/room1.png");
+    this.load.image("window", "../../assets/images/window.png");
 
     // load the tilemap exported from Tiled
-    this.load.tilemapTiledJSON('map', '../../assets/tilemaps/roomwithobject.json');
+    this.load.tilemapTiledJSON(
+        "map",
+        "../../assets/tilemaps/roomwithobject.json"
+    );
 }
 
 function create() {
     // parse the tilemap
-    map = this.make.tilemap({ key: 'map' });
+    map = this.make.tilemap({ key: "map" });
 
     // add the tileset image to the map
-    let tiles = map.addTilesetImage('room1', 'room1_img');
-    map.addTilesetImage('window', 'window');
+    let tiles = map.addTilesetImage("room1", "room1_img");
+    map.addTilesetImage("window", "window");
 
     // add the room_map layer to the scene
-    map.createStaticLayer('room_map', tiles, 0, 0);
+    map.createStaticLayer("room_map", tiles, 0, 0);
     // create sprites for the objects layer
-    map.createFromObjects('objects', 'window', { key: 'window' });
+    map.createFromObjects("objects", "window", { key: "window" });
 
-    this.input.on('pointerup', checkObjectSelection, this);
+    this.input.on("pointerup", checkObjectSelection, this);
 }
 
 function checkObjectSelection(pointer) {
-    let objectLayer = map.getObjectLayer('objects');
+    let objectLayer = map.getObjectLayer("objects");
     if (!objectLayer) return;
 
     // ensure we take the camera position into account
@@ -50,13 +53,13 @@ function checkObjectSelection(pointer) {
     let selectedObject = null;
     for (let object of objectLayer.objects) {
         // check if object is a polygon
-        if (object.hasOwnProperty('polyline')) {
+        if (object.hasOwnProperty("polyline")) {
             // each point on the polyline is relative to it's parent object's position,
             // so need to map them to world values
-            let points = object.polyline.map((value) => {
+            let points = object.polyline.map(value => {
                 return {
                     x: value.x + object.x,
-                    y: value.y + object.y,
+                    y: value.y + object.y
                 };
             });
             let polygon = new Phaser.Geom.Polygon(points);
@@ -65,11 +68,14 @@ function checkObjectSelection(pointer) {
                 selectedObject = object;
                 break;
             }
-        }
-        else {
+        } else {
             // not a polygon, so can just check against x,y,width,height
-            if (x >= object.x && x <= object.x + object.width
-                && y >= object.y - object.height && y <= object.y) {
+            if (
+                x >= object.x &&
+                x <= object.x + object.width &&
+                y >= object.y - object.height &&
+                y <= object.y
+            ) {
                 selectedObject = object;
                 break;
             }
@@ -81,17 +87,29 @@ function checkObjectSelection(pointer) {
         graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
         let topPadding = 50;
         let leftPadding = 40;
-        dialogBox = new Phaser.Geom.Rectangle(config.width-(config.width*11/12), config.height-(config.height*5/16), config.width*5/6, config.width*1/6);
+        dialogBox = new Phaser.Geom.Rectangle(
+            config.width - (config.width * 11) / 12,
+            config.height - (config.height * 5) / 16,
+            (config.width * 5) / 6,
+            (config.width * 1) / 6
+        );
         graphics.fillRectShape(dialogBox);
         var style = {
-          font: "24px Arial",
-          fill: "#000",
-          align: "left",
-          wordWrap: { width: 950, useAdvancedWrap: true },
-          "backgroundColor" : "#ffffff",
+            font: "24px Arial",
+            fill: "#000",
+            align: "left",
+            wordWrap: { width: 950, useAdvancedWrap: true },
+            backgroundColor: "#ffffff"
         };
-        var text = this.add.text(dialogBox.x+leftPadding, dialogBox.y+topPadding, selectedObject.properties['description'], style);
-        console.log('selected object description:' + selectedObject.properties['description']);
+        var text = this.add.text(
+            dialogBox.x + leftPadding,
+            dialogBox.y + topPadding,
+            selectedObject.properties["description"],
+            style
+        );
+        console.log(
+            "selected object description:" +
+                selectedObject.properties["description"]
+        );
     }
 }
-
