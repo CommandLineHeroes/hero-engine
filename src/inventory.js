460cee5948cdfbf39e9831d0265463af20304015
Ben <philipb.gregory@gmail.com>
2018-11-14 16:13:20 -0600

    Cleaning up lint some more

--- src/inventory.js
+++ src/inventory.js
@@ -5,13 +5,13 @@
 const item = class {
     /**
      * Create an item.
-     * @name - the name of the item.
-     * @attack - the attack power of the item.
-     * @armor - the armor class of the item.
-     * @weight - the weight of the item.
-     * @cost - the cost of the item.
-     * @amount - the amount of the item.
-     * @maxStackSize - defines the maximum stack size of the item.
+     * @param {string} name - the name of the item.
+     * @param {number} attack - the attack power of the item.
+     * @param {number} armor - the armor class of the item.
+     * @param {number} weight - the weight of the item.
+     * @param {number} cost - the cost of the item.
+     * @param {number} amount - the amount of the item.
+     * @param {number} maxStackSize - defines the maximum stack size of the item.
      */
     constructor(name, attack, armor, weight, cost, amount = 1, maxStackSize = 1) {
         this.name = name;
@@ -38,11 +38,12 @@ const inventory = class {
     
     /**
      * Add an item to the inventory.
+     * @param {class} item - the item to be manipulated.
      */
     add_item(item) {
         if (item !== null) {
             if (this.items[item.name]) {
-                this.items[item.name].amount += 1 ;
+                this.items[item.name].amount += 1;
             } else {
                 this.items[item.name] = item;
             }
@@ -51,17 +52,18 @@ const inventory = class {
     
      /**
       * Remove an item from the inventory.
+      * @param {class} item - the item to be manipulated.
       */
     remove_item(item) {
         if (item !== null) {
             if (this.items[item.name]) {
-                this.items[item.name].amount -= 1 ;
+                this.items[item.name].amount -= 1;
             } 
         }
     }
 
     /**
-     * Display the items in the inventory.
+     * @return {number} the items in the inventory.
      */
     display_items() {
         return this.items.join;
