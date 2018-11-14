d143eab37af0f4db5ee8dd06a067a4a9468d3a0a
Ben <philipb.gregory@gmail.com>
2018-11-14 15:48:07 -0600

    Tidying up
    This change introduces my attempts at ESLint to resolve some of the warnings/errors in the build. I have also introduced some recommended changes to the inventory system.

--- src/inventory.js
+++ src/inventory.js
@@ -1,24 +1,69 @@
+/** @constant
+    @type {class}
+    @default
+*/
 const item = class {
-    constructor(name, attack, armor, weight, cost) {
+    /**
+     * Create an item.
+     * @name - the name of the item
+     * @attack - the attack power of the item
+     * @armor - the armor class of the item
+     * @weight - the weight of the item
+     * @cost - the cost of the item
+     * @amount - the amount of the item
+     * @maxStackSize - defines the maximum stack size of the item.
+     */
+    constructor(name, attack, armor, weight, cost, amount = 1, maxStackSize = 1) {
         this.name = name;
         this.attack = attack;
         this.armor = armor;
         this.weight = weight;
         this.cost = cost;
+        this.amount = amount;
+        this.maxStackSize = maxStackSize;
     }
 };
 
+/** @constant
+    @type {class}
+    @default
+*/
 const inventory = class {
+    /**
+     * Create an inventory for items
+     */
     constructor() {
         this.items = [];
     }
-
+    
+    /**
+     * Add an item to the inventory
+     */
     add_item(item) {
-        this.items[item.name] = item;
+        if (item !== null) {
+            if (this.items[item.name]) {
+                this.items[item.name].amount += 1 ;
+            } else {
+                this.items[item.name] = item;
+            }
+        }
+    }
+    
+     /**
+      * Remove an item from the inventory
+      */
+    remove_item(item) {
+        if (item !== null) {
+            if (this.items[item.name]) {
+                this.items[item.name].amount -= 1 ;
+            } 
+        }
     }
 
+    /**
+     * Display the items in the inventory
+     */
     display_items() {
         return this.items.join;
     }
-
 };
