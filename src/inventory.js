fdbf5ae7418f2dcd37c0d8b8be282dcffa3731b4
Ben <philipb.gregory@gmail.com>
2018-11-15 22:54:14 -0600

    WIP - Get tests running
    This commit (finally) fixes my ESLint woes. I've added some tests and expectations, but am not getting the expected results. JS feels weird to me, but I'm getting the hang of it.

--- src/inventory.js
+++ src/inventory.js
@@ -13,13 +13,7 @@ const item = class {
      * @param {number} amount - the amount of the item.
      * @param {number} maxStackSize - defines the maximum stack size of the item.
      */
-    constructor(name, 
-                 attack, 
-                 armor, 
-                 weight, 
-                 cost, 
-                 amount = 1, 
-                 maxStackSize = 1) {
+    constructor(name, attack, armor, weight, cost, amount, maxStackSize) {
         this.name = name;
         this.attack = attack;
         this.armor = armor;
@@ -45,11 +39,19 @@ const inventory = class {
     /**
      * Add an item to the inventory.
      * @param {class} item - the item to be manipulated.
+     * @return {string} - a message if an item cannot be added.
      */
     add_item(item) {
         if (item !== null) {
             if (this.items[item.name]) {
-                this.items[item.name].amount += 1;
+                if (
+                    this.items[item.name].amount >=
+                    this.items[item.name].maxStackSize
+                ) {
+                    return 'No more of this item can be carried.';
+                } else {
+                    this.items[item.name].amount += 1;
+                }
             } else {
                 this.items[item.name] = item;
             }
@@ -57,9 +59,9 @@ const inventory = class {
     }
 
     /**
-      * Remove an item from the inventory.
-      * @param {class} item - the item to be manipulated.
-      */
+     * Remove an item from the inventory.
+     * @param {class} item - the item to be manipulated.
+     */
     remove_item(item) {
         if (item !== null) {
             if (this.items[item.name]) {
@@ -72,6 +74,40 @@ const inventory = class {
      * @return {number} the items in the inventory.
      */
     display_items() {
-        return this.items.join;
+        return Object.keys(this.items).join(', ');
     }
 };
+
+/**
+ * Some Tests
+ */
+const sword = new item('Sword', 5, 1, 3, 10, 1, 1);
+const potion = new item('Potion', 0, 0, 1, 3, 10);
+const heroInventory = new inventory();
+
+heroInventory.add_item(sword);
+heroInventory.add_item(potion);
+
+/**
+ * Expect 1 potion and 1 sword
+ */
+console.log(heroInventory.display_items());
+
+/**
+ * Expect that not all of these potions can be added
+ */
+for (let i = 0; i < 10; i++) {
+    heroInventory.add_item(potion);
+}
+
+/**
+ * Expect that the sword cannot be removed if it's not in the inventory
+ */
+for (let i = 0; i < 2; i++) {
+    heroInventory.remove_item(sword);
+}
+
+/**
+ * Expect 10 potions and 0 swords.
+ */
+console.log(heroInventory.display_items());
