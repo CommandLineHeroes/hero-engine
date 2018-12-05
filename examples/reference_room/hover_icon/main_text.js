fa738766a050e9455efa05dae825f543479ce4e9
Linus Probert <linus.probert@gmail.com>
2018-12-05 11:21:20 +0100

    Adds an example for mouse hover hints
    As described in #44

---
+++ examples/reference_room/hover_icon/main_text.js
@@ -0,0 +1,156 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 929,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+var text = null;
+let map = null;
+var graphics;
+var hintSprite = null;
+
+function preload() {
+    //TODO: Add Dialog Manager init here
+    // load the tileset image which is just a single tile for the background room
+    this.load.image("room1_img", "../../assets/images/room1.png");
+    this.load.image("window", "../../assets/images/window.png");
+    this.load.image("qmark", "../../assets/images/question_mark.png");
+
+    // load the tilemap exported from Tiled
+    this.load.tilemapTiledJSON(
+        "map",
+        "../../assets/tilemaps/roomwithobject.json"
+    );
+}
+
+function create() {
+    // parse the tilemap
+    map = this.make.tilemap({ key: "map" });
+
+    // add the tileset image to the map
+    let tiles = map.addTilesetImage("room1", "room1_img");
+    map.addTilesetImage("window", "window");
+
+    // add the room_map layer to the scene
+    map.createStaticLayer("room_map", tiles, 0, 0);
+    // create sprites for the objects layer
+    map.createFromObjects("objects", "window", { key: "window" });
+
+    hintSprite = this.add.sprite(0, 0, "qmark");
+    hintSprite.visible = false;
+
+    this.input.on("pointerup", checkObjectSelection, this);
+    this.input.on("pointermove", checkObjectHint, this);
+}
+
+function isPointOnObject(x, y, object) {
+    // check if object is a polygon
+    if (object.hasOwnProperty("polyline")) {
+        // each point on the polyline is relative to it's parent object's position,
+        // so need to map them to world values
+        let points = object.polyline.map(value => {
+            return {
+                x: value.x + object.x,
+                y: value.y + object.y
+            };
+        });
+        let polygon = new Phaser.Geom.Polygon(points);
+        let containsPoint = polygon.contains(x, y);
+        if (containsPoint) {
+            return true;
+        }
+    } else {
+        // not a polygon, so can just check against x,y,width,height
+        if (
+            x >= object.x &&
+            x <= object.x + object.width &&
+            y >= object.y - object.height &&
+            y <= object.y
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function checkObjectHint(pointer) {
+    let objectLayer = map.getObjectLayer("objects");
+    if (!objectLayer) return;
+
+    // ensure we take the camera position into account
+    let x = pointer.x - this.cameras.main.x;
+    let y = pointer.y - this.cameras.main.y;
+
+    console.log("Mouse motion:", x, y);
+
+    let selectedObject = null;
+    for (let object of objectLayer.objects) {
+        if (isPointOnObject(x, y, object)) {
+            selectedObject = object;
+            break;
+        }
+    }
+
+    if (!selectedObject || !selectedObject.properties["description"]) {
+        hintSprite.visible = false;
+        return;
+    }
+
+    hintSprite.visible = true;
+    hintSprite.x = x + 16;
+    hintSprite.y = y;
+}
+
+function checkObjectSelection(pointer) {
+    let objectLayer = map.getObjectLayer("objects");
+    if (!objectLayer) return;
+
+    // ensure we take the camera position into account
+    let x = pointer.x - this.cameras.main.x;
+    let y = pointer.y - this.cameras.main.y;
+
+    let selectedObject = null;
+    for (let object of objectLayer.objects) {
+        if (isPointOnObject(x, y, object)) {
+            selectedObject = object;
+            break;
+        }
+    }
+    if (selectedObject) {
+        //TODO: Dialog creation should happen here per requirements. We should also destroy or hide any other dialog currently displayed
+        //// WARNING: Hardcoding a minimal textbox to display.
+        graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
+        let topPadding = 50;
+        let leftPadding = 40;
+        dialogBox = new Phaser.Geom.Rectangle(
+            config.width - (config.width * 11) / 12,
+            config.height - (config.height * 5) / 16,
+            (config.width * 5) / 6,
+            (config.width * 1) / 6
+        );
+        graphics.fillRectShape(dialogBox);
+        var style = {
+            font: "24px Arial",
+            fill: "#000",
+            align: "left",
+            wordWrap: { width: 950, useAdvancedWrap: true },
+            backgroundColor: "#ffffff"
+        };
+        var text = this.add.text(
+            dialogBox.x + leftPadding,
+            dialogBox.y + topPadding,
+            selectedObject.properties["description"],
+            style
+        );
+        console.log(
+            "selected object description:" +
+                selectedObject.properties["description"]
+        );
+    }
+}
